d6c961b119e001c9c1d65d4a184048f402babac5
yaginumatti <ryohei122yagi@gmail.com>
2023-04-23 18:10:49 +0900

    固定表示文字列の国際化

---
+++ Todolist8/src/main/java/com/example/todolist/Todolist8Application.java
@@ -0,0 +1,13 @@
+package com.example.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todolist8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Todolist8Application.class, args);
+	}
+
+}
